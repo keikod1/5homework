2145658ab8434fe086dd8e7c0eba0e5c5b127827
keikod1 <madragimova@icloud.com>
2021-11-19 08:57:49 +0600

    home work 2

--- 5homework/main.swift
+++ 5homework/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Введите номер телефона:")
 
+var numbers: [Int: String] = [
+    0509050399: "Adina",
+    0505010101: "Dastan",
+    0505050505: "Nursuktan",
+    0501010101: "Rawit",
+]
+
+var searchNumbers = readLine()
+var search = true
+
+for (key, value) in numbers{
+    if (Int(searchNumbers!) ?? 0) == key{
+        search = true
+        print( "звонок от: \(value)" )
+        break
+    }else{
+        search = false
+    
+    }
+}
+
+
+var serch2 = true
+
+while serch2{
+    if search == false{
+        print("звонок от: \(searchNumbers!)")
+        break
+    }
+}
